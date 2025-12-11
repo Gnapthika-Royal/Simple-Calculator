ed0c2747de0e15473fbdb5b34e144e5f4c4c36af
Gnapthika-Royal <gnapthika9@gmail.com>
2025-12-11 22:13:08 +0530

    Update SimpleCalculator.c

--- SimpleCalculator.c
+++ SimpleCalculator.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 #include <float.h>
-
 int main() {
     char op;
     double a, b, res;
-
-    // Read the operator
     printf("Enter an operator (+, -, *, /): ");
     scanf("%c", &op);
-
-    // Read the two numbers
     printf("Enter two operands: ");
     scanf("%lf %lf", &a, &b);
-    
-    // Define all four operations in the corresponding
-    // switch-case
     switch (op) {
     case '+':
         res = a + b;
@@ -34,6 +26,5 @@ int main() {
     }
     if(res!=-DBL_MAX)
       printf("%.2lf", res);
-    
-    return 0;
+      return 0;
 }
